0847b606f1fd2da155ac69b22707aa64192e8fd6
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-10-13 22:19:57 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  // run on browser only
+
+// null=> standalone value ,its representation of empty value
+// undefined => value not assigned
+// symbol => unique
+
+let age=138
+
+//  object 
+
+console.log(typeof null)
+console.log(typeof undefined)
