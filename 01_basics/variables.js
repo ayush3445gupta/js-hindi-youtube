ce959db8c48169bf3b5bb060124eebb775ab1649
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-10-13 22:20:34 +0000

    datatype

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const ID = 8743
+let email = 'sdhjas8724'
+var password = '3429'
+accountcity = 'Varanasi' // not preffered
+ 
+
+// ID = 3 not allowed
+email = 'hsdfff'
+password='8937'
+
+/*
+
+*/
+console.log(ID)
+console.table([email,password])
